c346777727b511907b9396f702d658d4a05ca522
terumi8.neu@icloud.com <terumi8.neu@icloud.com>
2022-03-26 20:19:25 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState } from 'react';
+
 import './App.css'
 
 /**
@@ -8,9 +10,26 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setdogUrl] = useState("https://images.dog.ceo/breeds/otterhound/n02091635_1311.jpg");
+
+  const changeDog = async() =>{
+  let response =  await fetch('https://dog.ceo/api/breeds/image/random');
+  let commits =  await response.json(); 
+  let url = commits.message;
+  setdogUrl(url);
+  }
+  
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+    <header>犬の画像を見せましょう！</header>
+    <body>
+      <title>可愛い犬の画像です</title>
+      <main>
+      <img className="dogimage" src={dogUrl} alt="可愛い犬の画像です" />
+      <button className="chbutton" onClick={() =>changeDog()}>次の犬</button>
+      </main>
+    </body>
+    <footer/>
+    </>
   )
 }
